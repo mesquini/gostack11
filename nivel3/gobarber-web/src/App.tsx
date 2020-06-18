f5e86f7a100643fe7fc42686d85628f6a2ef0cde
victor.mesquini <victor.mesquini@ingaia.com.br>
2020-06-18 12:05:36 -0300

    add project web

---
+++ nivel3/gobarber-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>olaaaa</h1>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
