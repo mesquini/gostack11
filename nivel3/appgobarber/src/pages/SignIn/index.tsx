7dcc7d91a5d45ed8969bd3cbfae243a7575642f6
mesquini <mesquini@live.com>
2020-06-22 23:40:39 -0300

    create mobile gobarber

---
+++ nivel3/appgobarber/src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SingIn: React.FC = () => {
+  return (
+    <Container>
+      <Text>Singh</Text>
+    </Container>
+  );
+};
+
+export default SingIn;
